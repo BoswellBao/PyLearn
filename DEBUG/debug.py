7f2c9830cc77113896430fddf060f2970c87648b
BoswellBao <277532844@qq.com>
2017-07-14 17:50:57 +0800

    testlink1

---
+++ DEBUG/debug.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+from testlink.testlinkapi  import TestlinkAPIClient
+from testlink.testlinkhelper import TestLinkHelper
+from testlink.testlinkerrors import TLResponseError
+import sys, os.path
+from platform import python_version
+
+
+
+tl_helper = TestLinkHelper()
